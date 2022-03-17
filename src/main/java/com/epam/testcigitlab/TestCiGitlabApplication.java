7644990df22eff89b9052d0f7e8f33a33627170a
Artur_Mkrtumyan <mkrtart9211@gmail.com>
2022-03-17 22:42:55 +0400

    test

--- src/main/java/com/epam/testcigitlab/TestCiGitlabApplication.java
+++ src/main/java/com/epam/testcigitlab/TestCiGitlabApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 public class TestCiGitlabApplication {
+
     @GetMapping
     public String test(){
         return "Hello gitlab CI";
