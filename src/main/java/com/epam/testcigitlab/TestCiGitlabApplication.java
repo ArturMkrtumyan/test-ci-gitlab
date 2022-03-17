7626b25eed855abd228dd5f8893cee8df1077cfe
Artur_Mkrtumyan <mkrtart9211@gmail.com>
2022-03-17 22:40:27 +0400

    Initial commit

---
+++ src/main/java/com/epam/testcigitlab/TestCiGitlabApplication.java
@@ -0,0 +1,22 @@
+package com.epam.testcigitlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping
+public class TestCiGitlabApplication {
+    @GetMapping
+    public String test(){
+        return "Hello gitlab CI";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCiGitlabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/testcigitlab/TestCiGitlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.testcigitlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCiGitlabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
